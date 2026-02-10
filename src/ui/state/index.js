7fb8e3be486794111da2bdfec8d35aa23f9e25fb
humansinstitute <petewinn@gmail.com>
2026-02-10 08:35:28 +0800

    Add Send 4 and Send 5 to terminal control actions

--- src/ui/state/index.js
+++ src/ui/state/index.js
@@ -39,6 +39,8 @@ export const TERMINAL_CONTROL_ACTIONS = [
   { id: "terminal-1", label: "Send 1", toastLabel: "1", sequence: "1" },
   { id: "terminal-2", label: "Send 2", toastLabel: "2", sequence: "2" },
   { id: "terminal-3", label: "Send 3", toastLabel: "3", sequence: "3" },
+  { id: "terminal-4", label: "Send 4", toastLabel: "4", sequence: "4" },
+  { id: "terminal-5", label: "Send 5", toastLabel: "5", sequence: "5" },
   { id: "terminal-up", label: "Send Arrow Up", toastLabel: "Arrow Up", sequence: "\u001b[A" },
   { id: "terminal-down", label: "Send Arrow Down", toastLabel: "Arrow Down", sequence: "\u001b[B" },
   { id: "terminal-shift-tab", label: "Send Shift+Tab", toastLabel: "Shift+Tab", sequence: "\u001b[Z" },
